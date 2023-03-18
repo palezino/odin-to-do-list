0a320f05d624516b056ea483b987e8d13f8321e1
Daniil Panov <from4life@pm.me>
2023-03-18 13:28:15 +0700

    Create manageTasks() function to open and close tasks edit form

--- src/index.js
+++ src/index.js
@@ -1,78 +1,22 @@
-const fns = require('date-fns');
+import { tasksFactory, manageTasks } from "./script-tasks";
 
+const fns = require("date-fns");
 
-console.log('hello');
+console.log("hello");
+const mainTasks = document.querySelector(".main-tasks");
 
-// create and display tasks
-
-const mainTasks = document.querySelector('.main-tasks');
-
-const createNewElement = (element, className, text=null) => {
-    const newElement = document.createElement(element);
-    newElement.classList.add(className);
-    newElement.innerText = text;
-    return newElement;
-}
+const createNewElement = (element, className, text = null) => {
+  const newElement = document.createElement(element);
+  newElement.classList.add(className);
+  newElement.innerText = text;
+  return newElement;
+};
 
 const appendChildren = (parent, children) => {
-    children.forEach(child => {
-        parent.appendChild(child);
-    });
-}
-
-const tasksFactory = (name, dueDate=null, priority=null, description=null) => {
-    const task = createNewElement('div', 'task');
-    const taskChildren = [
-        createNewElement('div', 'task-priority'),
-        createNewElement('label', 'task-title', name),
-        createNewElement('div', 'task-date'),
-        createNewElement('div', 'task-edit'),
-        createNewElement('div', 'task-delete')
-    ];
-
-    const taskTitleChildren = [];
-    // checkbox element
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    const checkmark = createNewElement('span', 'checkmark');
-    taskTitleChildren.push(checkbox);
-    taskTitleChildren.push(checkmark);
-    appendChildren(taskChildren[1], taskTitleChildren);
-    // date
-
-
-    // edit icon
-    const editImg = document.createElement('img');
-    editImg.setAttribute('src', './images/edit-3.svg');
-    editImg.setAttribute('alt', 'edit');
-    taskChildren[3].appendChild(editImg);
-    // delete icon
-    const deleteImg = document.createElement('img');
-    deleteImg.setAttribute('src', './images/x-square.svg');
-    deleteImg.setAttribute('alt', 'delete');
-    taskChildren[4].appendChild(deleteImg);
-
-    appendChildren(task, taskChildren);
-
-    return task;
-}
-
-// create and display notes
-const notesFactory = (title, text=null) => {
-    const note = createNewElement('div', 'note');
-    const noteChildren = [
-        createNewElement('img', 'delete-note'),
-        createNewElement('h4', 'note-title', title),
-        createNewElement('p', 'note-text', text)
-    ];
-
-    noteChildren[0].setAttribute('src', './images/x.svg');
-    noteChildren[0].setAttribute('alt', 'delete-note');
-
-    appendChildren(note, noteChildren);
-
-    return note;
-}
+  children.forEach((child) => {
+    parent.appendChild(child);
+  });
+};
 
 // // a template to make links work
 // const sidebarList = document.querySelector('.sidebar-list');
@@ -85,69 +29,111 @@ const notesFactory = (title, text=null) => {
 // })
 
 // display the date in the header
-const today = fns.format(new Date(), 'EEEE, MMM d y');
-const dateDisplay = document.querySelector('.today-date');
-
+const today = fns.format(new Date(), "EEEE, MMM d y");
+const dateDisplay = document.querySelector(".today-date");
 dateDisplay.innerText = today;
 
 // open and close pop-up forms (create and edit)
-const plusBtn = document.querySelector('.plus-btn');
+// create form
+const plusBtn = document.querySelector(".plus-btn");
 
-plusBtn.addEventListener('click', () => {
-    document.querySelector('.bg-form').style.display = 'flex';
-    document.querySelector('.todo-form').style = "transform: scale(1.1); font-weight: 700;";
-})
+plusBtn.addEventListener("click", () => {
+  document.querySelector(".bg-form").style.display = "flex";
+  document.querySelector(".todo-form").style =
+    "transform: scale(1.1); font-weight: 700;";
+});
 
-const bgForm = document.querySelector('.bg-form');
+const bgForm = document.querySelector(".bg-form");
 
-bgForm.addEventListener('click', (e) => {
-    // console.log(e.target.className);
-    if (e.target.className === "bg-form" || e.target.className === "close-form") {
-        bgForm.style.display = 'none';
-        document.querySelector(".task-edit-bg").style.display = 'none';
-    }
-})
+bgForm.addEventListener("click", (e) => {
+  // console.log(e.target.className);
+  if (e.target.className === "bg-form" || e.target.className === "close-form") {
+    bgForm.style.display = "none";
+    document.querySelector(".task-edit-bg").style.display = "none";
+  }
+});
 
-bgForm.addEventListener('keydown', (e) => {
-    // console.log(e.keyCode);
-    if (e.keyCode === 27) {
-        bgForm.style.display = 'none';
-    }
-})
+bgForm.addEventListener("keydown", (e) => {
+  // console.log(e.keyCode);
+  if (e.keyCode === 27) {
+    bgForm.style.display = "none";
+  }
+});
 
-const taskEditBtn = document.querySelector('.task-edit');
-const taskEditForm = document.querySelector('.task-edit-bg');
+// edit form
+manageTasks().openEditForm();
+manageTasks().closeEditForm();
 
-taskEditBtn.addEventListener('click', () => {
-    taskEditForm.style.display = 'flex';
-})
 
-taskEditForm.addEventListener('click', (e) => {
-    if (e.target.className === "task-edit-bg" || e.target.className === "close-form") {
-        taskEditForm.style.display = "none";
-    }
-})
 
-// delete targeted task
-document.querySelectorAll('.task-delete').forEach(btn => btn.addEventListener('click', (e) => {
-    mainTasks.removeChild(e.target.parentElement.parentElement);
-}));
 
 // working links in pop-up form
-const sidebarListForm = document.querySelector('.sidebar-list-form');
+const sidebarListForm = document.querySelector(".sidebar-list-form");
 
-sidebarListForm.addEventListener('click', (e) => {
-    // console.log(e.target.textContent);
-    if (e.target.textContent === 'TO-DO') {
-        document.querySelector('.note-form').style = "transform: scale(1); font-weight: 400;";
-        document.querySelector('.new-note-form').style.display = 'none';
-        document.querySelector('.new-task-form').style.display = 'flex';
-        document.querySelector('.todo-form').style = "transform: scale(1.1); font-weight: 700;";
-    } 
-    if (e.target.textContent === 'Note') {
-        document.querySelector('.todo-form').style = "transform: scale(1); font-weight: 400;";
-        document.querySelector('.new-task-form').style.display = 'none';
-        document.querySelector('.new-note-form').style.display = 'flex';
-        document.querySelector('.note-form').style = "transform: scale(1.1); font-weight: 700;";
-    }
+sidebarListForm.addEventListener("click", (e) => {
+  // console.log(e.target.textContent);
+  if (e.target.textContent === "TO-DO") {
+    document.querySelector(".note-form").style =
+      "transform: scale(1); font-weight: 400;";
+    document.querySelector(".new-note-form").style.display = "none";
+    document.querySelector(".new-task-form").style.display = "flex";
+    document.querySelector(".todo-form").style =
+      "transform: scale(1.1); font-weight: 700;";
+  }
+  if (e.target.textContent === "Note") {
+    document.querySelector(".todo-form").style =
+      "transform: scale(1); font-weight: 400;";
+    document.querySelector(".new-task-form").style.display = "none";
+    document.querySelector(".new-note-form").style.display = "flex";
+    document.querySelector(".note-form").style =
+      "transform: scale(1.1); font-weight: 700;";
+  }
+});
+
+
+// hide create and delete tasks in IIFE module?
+// delete targeted task
+// function deleteTask() {
+//   document.querySelectorAll(".task-delete").forEach((btn) =>
+//     btn.addEventListener("click", (e) => {
+//       mainTasks.removeChild(e.target.parentElement.parentElement);
+//     })
+//   );
+// }
+// deleteTask();
+
+// create tasks
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', () => {
+  mainTasks.appendChild(tasksFactory());
+  bgForm.style.display = "none";
+  manageTasks().deleteTask();
+  // manageTasks().openEditForm();
 })
+
+
+
+
+
+// testing priority
+
+// submitBtn.addEventListener('click', () => {
+//     document.querySelectorAll('.priority-input').forEach((radioBtn) => {
+//         // console.log(radioBtn.checked);
+//         if (radioBtn.checked) {
+//             console.log(radioBtn.value);
+//         }
+//     });
+// })
+
+// testing date input
+// submitBtn.addEventListener('click', () => {
+//     console.log(fns.format(new Date(document.querySelector('.due-date-input').value), "EEEE, MMM d y"));
+// })
+
+// testing title input
+// submitBtn.addEventListener('click', () => {
+//     console.log(document.querySelector('.task-title-form').value);
+// })
+
