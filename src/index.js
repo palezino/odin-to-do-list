c08634bba5eb8dcfe06d6a1f5dab99548841579d
Daniil Panov <from4life@pm.me>
2023-03-27 13:21:42 +0700

    Edit button works and lets edit task immediately after being editted

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
   createDefaultToDos,
   sortToLatestDate,
   sortTasks,
+  sortTasksByProj
 } from "./script-tasks";
 
 const fns = require("date-fns");
@@ -94,21 +95,23 @@ bgForm.addEventListener("keydown", (e) => {
   }
 });
 
-// navigate links in edit form
-manageTasks().navigateEditForm();
+// navigate links in new task form
+manageTasks().navigateForm();
 
 // create tasks
 const submitBtn = document.querySelector(".submit-btn");
 
 submitBtn.addEventListener("click", () => {
-  mainTasks.appendChild(tasksFactory());
+  // mainTasks.appendChild(tasksFactory());
   // removeChildren(mainTasks);
   // tasksList.push(tasksFactory());
   tasksList.splice(0, tasksList.length);
   tasksList = [...mainTasks.childNodes];
+  tasksList.push(tasksFactory());
+  
   sortToLatestDate(tasksList);
   sortTasks(tasksList);
-  // appendChildren(mainTasks, tasksList);
+  appendChildren(mainTasks, tasksList);
   bgForm.style.display = "none";
 
   manageTasks().deleteTask();
@@ -116,7 +119,43 @@ submitBtn.addEventListener("click", () => {
   manageTasks().closeEditForm();
 });
 
-// console.log(tasksList[0].childNodes[0].style.backgroundColor);
+// edit a task
+(() => {
+  const editButton = document.querySelector('.submit-btn-edit');
+  const taskToBeEdited = manageTasks().openEditForm();
+  editButton.addEventListener('click', () => {
+    // existing values
+    let taskTitle = taskToBeEdited[0];
+    let taskDetails = taskToBeEdited[1]
+    let taskDate = fns.format(new Date(taskToBeEdited[2]), "EEEE, MMM d y");
+    let taskProject = taskToBeEdited[3]
+    let taskPriority = taskToBeEdited[4];
+    // edited inputs
+    const editTaskTitle = document.querySelector('.task-title-edit');
+    const editTaskDetails = document.querySelector('.task-text-edit');
+    const editTaskDate = document.querySelector('.due-date-input-edit');
+    const editTaskProj = document.querySelector('#project-name-edit');
+    // inserting edited values
+    taskTitle = editTaskTitle.value;
+    taskDetails = editTaskDetails.value;
+    taskDate = fns.format(new Date(editTaskDate.value), "EEEE, MMM d y");
+    taskProject = editTaskProj.value;
+    document.querySelectorAll('.priority-edit').forEach((radioBtn) => {
+        if (radioBtn.checked) {
+          taskPriority = radioBtn.value;
+        }
+    })
+
+    document.querySelector(".main-tasks").removeChild(taskToBeEdited[5]);
+    document.querySelector(".main-tasks").appendChild(createDefaultToDos(taskPriority, taskTitle, taskDate, taskDetails, taskProject));
+    document.querySelector(".task-edit-bg").style.display = "none";
+
+    manageTasks().deleteTask();
+    manageTasks().openEditForm();
+    manageTasks().closeEditForm();
+
+  })
+})();
 
 // navigate home/today/week/month pages
 
@@ -173,19 +212,9 @@ const enableProjectInput = () => {
 
 enableProjectInput();
 
-const sortTasksByProj = () => {
-  const sortedProj = [];
-  // eslint-disable-next-line guard-for-in, no-restricted-syntax
-  for (const i in tasksList) {
-    const proj = [tasksList[i].childNodes[6].innerText, tasksList[i]];
-    sortedProj.push(proj);
-  }
-  return sortedProj;
-};
-
 // navigate among projects
 document.querySelector('.sidebar-projects').addEventListener('click', (e) => {
-  const projectsList = sortTasksByProj();
+  const projectsList = sortTasksByProj(tasksList);
   const sortedProjects = [];
   projectsList.forEach(item => {
     if (item[0] === e.target.innerText) {
@@ -196,6 +225,143 @@ document.querySelector('.sidebar-projects').addEventListener('click', (e) => {
   appendChildren(mainTasks, sortedProjects);
 });
 
+// manageTasks().editForm()
+
+// const manageTasks = (() => {
+//   const taskEditBtns = document.querySelectorAll(".task-edit");
+//   const taskEditForm = document.querySelector(".task-edit-bg");
+//   const sidebarListForm = document.querySelector(".sidebar-list-form");
+
+//   const closeEditForm = () => {
+//     taskEditForm.addEventListener("click", (e) => {
+//       if (
+//         e.target.className === "task-edit-bg" ||
+//         e.target.className === "close-edit"
+//       ) {
+//         taskEditForm.style.display = "none";
+//       }
+//     });
+//   };
+
+//   const deleteTask = () => {
+//     document.querySelectorAll(".task-delete").forEach((btn) =>
+//       btn.addEventListener("click", (e) => {
+//         document.querySelector(".main-tasks").removeChild(e.target.parentElement.parentElement);
+//       })
+//     );
+//   };
+
+//   const openEditForm = () => {
+//     const taskToBeEdited = [];
+//     taskEditBtns.forEach((btn) => btn.addEventListener('click', (e) => {
+//       taskEditForm.style.display = "flex";
+//       // taskToBeEdited.splice(0, taskToBeEdited.length);
+//       // existing values
+//       const taskElement = e.target.parentElement.parentElement;
+//       const taskTitle = e.target.parentElement.parentElement.childNodes[1].childNodes[1].innerText;
+//       const taskDetails = e.target.parentElement.parentElement.childNodes[5].innerText;
+//       let taskDate = e.target.parentElement.parentElement.childNodes[2].innerText;
+//       taskDate = Date.parse(taskDate);
+//       const taskProject = e.target.parentElement.parentElement.childNodes[6].innerText;
+//       const taskPriority = e.target.parentElement.parentElement.childNodes[0].childNodes[0].innerText;
+//       taskToBeEdited.push(taskTitle, taskDetails, taskDate, taskProject, taskPriority, taskElement);
+
+//       // select edit-form inputs
+//       const editTaskTitle = document.querySelector('.task-title-edit');
+//       const editTaskDetails = document.querySelector('.task-text-edit');
+//       const editTaskDate = document.querySelector('.due-date-input-edit');
+//       const editTaskProj = document.querySelector('#project-name-edit');
+//       const editTaskPriority = document.querySelector(`#priority-edit-${taskPriority}`);
+//       // insert existing values
+//       editTaskTitle.value = taskTitle;
+//       editTaskDetails.value = taskDetails;
+//       editTaskDate.value = fns.format(new Date(taskDate), 'yyyy-MM-dd');
+//       if (taskProject === 'undefined') {
+//         editTaskProj.value = null;
+//       } else {
+//         editTaskProj.value = taskProject;
+//       }
+//       editTaskPriority.checked = true;
+
+//       // confirm edits
+//       // const editButton = document.querySelector('.submit-btn-edit');
+//       // editButton.addEventListener('click', () => {
+//       //   taskTitle = editTaskTitle.value;
+//       //   taskDetails = editTaskDetails.value;
+//       //   taskDate = fns.format(new Date(editTaskDate.value), "EEEE, MMM d y");
+//       //   taskProject = editTaskProj.value;
+        
+//       //   document.querySelectorAll('.priority-edit').forEach((radioBtn) => {
+//       //       if (radioBtn.checked) {
+//       //         taskPriority = radioBtn.value;
+//       //       }
+//       //   })
+
+//       //   document.querySelector(".main-tasks").removeChild(e.target.parentElement.parentElement);
+//       //   document.querySelector(".main-tasks").appendChild(createDefaultToDos(taskPriority, taskTitle, taskDate, taskDetails, taskProject));
+//       //   taskEditForm.style.display = "none";
+//       // })
+//     }));
+//     return taskToBeEdited;
+//   };
+
+//   const editForm = () => {
+//     const editButton = document.querySelector('.submit-btn-edit');
+//     const taskToBeEdited = openEditForm();
+//     editButton.addEventListener('click', () => {
+//       // existing values
+//       let taskTitle = taskToBeEdited[0];
+//       let taskDetails = taskToBeEdited[1]
+//       let taskDate = fns.format(new Date(taskToBeEdited[2]), "EEEE, MMM d y");
+//       let taskProject = taskToBeEdited[3]
+//       let taskPriority = taskToBeEdited[4];
+//       // edited inputs
+//       const editTaskTitle = document.querySelector('.task-title-edit');
+//       const editTaskDetails = document.querySelector('.task-text-edit');
+//       const editTaskDate = document.querySelector('.due-date-input-edit');
+//       const editTaskProj = document.querySelector('#project-name-edit');
+//       // inserting edited values
+//       taskTitle = editTaskTitle.value;
+//       taskDetails = editTaskDetails.value;
+//       taskDate = fns.format(new Date(editTaskDate.value), "EEEE, MMM d y");
+//       taskProject = editTaskProj.value;
+//       document.querySelectorAll('.priority-edit').forEach((radioBtn) => {
+//           if (radioBtn.checked) {
+//             taskPriority = radioBtn.value;
+//           }
+//       })
+
+//       document.querySelector(".main-tasks").removeChild(taskToBeEdited[5]);
+//       document.querySelector(".main-tasks").appendChild(createDefaultToDos(taskPriority, taskTitle, taskDate, taskDetails, taskProject));
+//       taskEditForm.style.display = "none";
+//     })
+//   }
+
+//   const navigateForm = () => {
+//     sidebarListForm.addEventListener("click", (e) => {
+//       if (e.target.textContent === "TO-DO") {
+//         document.querySelector(".note-form").style =
+//           "transform: scale(1); font-weight: 400;";
+//         document.querySelector(".new-note-form").style.display = "none";
+//         document.querySelector(".new-task-form").style.display = "flex";
+//         document.qtuerySelector(".todo-form").style =
+//           "transform: scale(1.1); font-weight: 700;";
+//       }
+//       if (e.target.textContent === "Note") {
+//         document.querySelector(".todo-form").style =
+//           "transform: scale(1); font-weight: 400;";
+//         document.querySelector(".new-task-form").style.display = "none";
+//         document.querySelector(".new-note-form").style.display = "flex";
+//         document.querySelector(".note-form").style =
+//           "transform: scale(1.1); font-weight: 700;";
+//       }
+//     });
+//   };
+
+  
+
+//   return {openEditForm, closeEditForm, deleteTask, navigateForm, editForm};
+// })();
 
 // console.log(sortTasksByProj())
 
